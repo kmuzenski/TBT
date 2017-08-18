cb544b72a905cf152a3392ea7e1ed3e1024b4316
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-08-17 23:16:07 -0500

    +resources page

--- nav.php
+++ nav.php
@@ -9,13 +9,13 @@
   
   <div class="col-xs-12 col-md-4 col-lg-4" id="navborder" >
   <center>
-  <p><a href="gallery.php" id="mynav" title="gallery">Gallery</a></p>
+  <p><a href="resources.php" id="mynav" title="gallery">Resources</a></p>
   </center>
   </div>
 
   <div class="col-xs-12 col-md-4 col-lg-4" id="navborder" >
   <center>
-  <p><a href="contact.php" id="mynav" title="contact" id="mynav">Contact</a></p>
+  <p><a href="forums.php" id="mynav" title="contact" id="mynav">Forums</a></p>
   </center>
   </div>
 
---
+++ resources.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+
+<?php require_once('header.php'); ?>
+
+<body>
+<?php require_once('nav.php'); ?>
+<br><br><br><br><br>
+
+<center>
+<div class="container" id="dborder">
+<div class="row">
+<div class="col-xs-6 col-md-6 col-lg-6">
+
+<h3>Search Locally</h3>
+
+</div>
+</div>
+</div> <!--container-->
+<br>
+<div class="container" id="dborder">
+<div class="row">
+<div class="col-xs-6 col-md-6 col-lg-6">
+
+<h3>National Resources</h3>
+
+</div>
+</div>
+</div><!--container-->
+
+</center>
+<br><br><br><br><br>
+<?php require_once('footer.php'); ?>
+
+</body>
+</html>
