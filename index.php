7507b9d2690041a2882b84721f16be12ebb18fc0
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-08-17 22:45:42 -0500

    testing img

--- index.php
+++ index.php
@@ -8,8 +8,18 @@
 <?php require_once('nav.php'); ?>
 <br><br><br><br><br>
 
+<center>
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-md-4 col-lg-4">
 
+<img src="assets/img/bubs.jpg">
 <p>#TBT<p>
+
+</div>
+</div>
+</div> <!--container-->
+</center>
 <br><br><br><br><br>
 <?php require_once('footer.php'); ?>
 
